0dea45e28123151c9c0d1fcbfa60a2d3f7754b2d
Muhammad Faheem <157746418+muhmmadFaheem@users.noreply.github.com>
2024-03-21 03:01:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: "Select One of the Operators to perform for action", type: "list",
+        name: "Operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
+//Conditional statement..
+if (answer.Operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.Operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.Operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.Operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Operator Please Try Again");
+}
